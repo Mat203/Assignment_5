74f69540bb555ee4c6c5008c777d43a921d7b162
Mat_Tol <matvey.talalaevskiy@gmail.com>
2023-11-09 21:56:16 +0200

    decode by using most frequent symbol

--- task_5.py
+++ task_5.py
@@ -1,15 +1,18 @@
+import random
+
 def caesar_encrypt(text, shift):
-    shift = shift % 128
+    shift = shift % 95  
     encrypted_text = []
     for char in text:
-        if char != '\n':
-            ascii_value = ord(char)  
-            shifted_value = ascii_value + shift
-            encrypted_char = chr(shifted_value)  
+        if 32 <= ord(char) <= 126: 
+            ascii_value = ord(char) - 32  
+            shifted_value = (ascii_value + shift) % 95  
+            encrypted_char = chr(shifted_value + 32) 
             encrypted_text.append(encrypted_char)
         else:
             encrypted_text.append(char)
     return ''.join(encrypted_text)
+
 
 def caesar_decrypt(text, shift):
     shift = shift % 128
@@ -32,18 +35,18 @@ def write_file(file_path, content):
 
 
 def apply_caesar_operation(operation_function, shift):
-    file_path = input("Enter the file name to read")
+    file_path = input("Enter the file name to read: ")
     text = read_file(file_path)
 
     result_text = operation_function(text, shift)
-    output_file_path = input("Enter the file name to write")
+    output_file_path = input("Enter the file name to write: ")
 
     write_file(output_file_path, result_text)
     print("Operation is completed")
 
 def main():
     operation = input("Enter 'e' for encrypt or 'd' for decrypt: ")
-    shift = int(input("Enter shift: "))
+    shift = random.randint(1, 20)
     
     if operation.lower() == 'e':
         apply_caesar_operation(caesar_encrypt, shift)
---
+++ task_6.py
@@ -0,0 +1,41 @@
+import matplotlib.pyplot as plt
+from collections import Counter
+from task_5 import caesar_decrypt
+
+def read_file(file_path):
+    with open(file_path, 'r') as file:
+        text = file.read().replace('\n', '')
+    return text
+
+def calculate_frequencies(text):
+    return Counter(text)
+
+def find_most_frequent_letter(letter_freq):
+    return letter_freq.most_common(1)[0][0]
+
+def calculate_shift(most_frequent_letter, most_frequent_english_symbol=' '):
+    return ord(most_frequent_letter) - ord(most_frequent_english_symbol)
+
+def plot_frequencies(letter_freq):
+    letters = list(letter_freq.keys())
+    freq = list(letter_freq.values())
+    plt.figure(figsize=(10, 5))
+    plt.bar(letters, freq)
+    plt.xlabel('Letters')
+    plt.ylabel('Frequency')
+    plt.title('Frequency of each letter')
+    plt.show()
+
+def main():
+    text = read_file('123456.txt')
+    letter_freq = calculate_frequencies(text)
+    most_frequent_letter = find_most_frequent_letter(letter_freq)
+    print(most_frequent_letter)
+    shift = calculate_shift(most_frequent_letter)
+    print(shift)
+    decrypted_text = caesar_decrypt(text, shift)
+    print(decrypted_text)
+    plot_frequencies(letter_freq)
+
+if __name__ == "__main__":
+    main()
